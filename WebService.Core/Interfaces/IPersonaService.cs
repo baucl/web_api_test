99b01af415bef91350fdc1bb184ae40938d19145
Bautista <israelcll1994@gmail.com>
2021-07-02 09:10:13 -0400

    commit

---
+++ WebService.Core/Interfaces/IPersonaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebService.API.Model;
+using WebService.Domain.Views;
+
+namespace WebService.Core.Interfaces
+{
+    public interface IPersonaService
+    {
+        public Task<List<Persona>> GetPersonasService();
+        public Persona GetPersonaByIdService(Guid id);
+        public Persona CreatePersonaService(PersonaDto persona);
+        public Persona UpdatePersonaService(Persona persona);
+        public Task<bool> DeletePersonaService(Guid id);
+    }
+}
---
+++ WebService.Domain/Interfaces/IPersonaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebService.API.Model;
+
+namespace WebService.Domain.Interfaces
+{
+    public interface IPersonaRepository
+    {
+        public Task<List<Persona>> GetPersonasRepository();
+        public Persona GetPersonByIdRepository(Guid id);
+        public Persona CreatePersonaRepository(Persona persona);
+        public Persona UpdatePersonaRepository(Persona persona);
+        public Task<bool> DeletePersonaRepository(Guid id);
+
+    }
+}
